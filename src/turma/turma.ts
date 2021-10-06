e83b06c6ccaf7d2a2af52bcbb1eac489d59c194e
DaniellaMagalhaesSiqueira <daniella.siqueira80@gmail.com>
2021-10-06 20:45:21 -0300

    feat: adicionar aluno na turma

--- src/turma/turma.ts
+++ src/turma/turma.ts
@@ -1,5 +1,7 @@
 import { ALAN_JHONNES } from "../alunos/alan-jhonnes";
+import { DANIELLA_SIQUEIRA } from "../alunos/daniella-siqueira";
 
 export const turma: string[] = [
     ALAN_JHONNES,
+    DANIELLA_SIQUEIRA,
 ]
